29089302d8cbae57f36026f52ff404cea5c2e28c
Matt <matt@forefathersgroup.com>
2018-07-02 08:26:04 -0400

    Homepage start

--- public/page-home.php
+++ public/page-home.php
@@ -3,7 +3,160 @@ include('header.php');
 include('components/header.php');
 ?>
 
+<section class="home-page">
 
+<!--Start: Section One-->
+<div class="home-section section-one">
+  <h1 class="lead-txt">
+    <p>Journey down river into the heart of Sichuan Discover a land of fiery flavors Taste tradition</p>
+    <a href="" class="arrow-dwn"></a>
+  </h1>
+</div>
+<!--End: Section One-->
+
+
+<!--Start: Section Two-->
+<div class="home-section section-two">
+
+  <div class="content-block">
+    <div class="scroll-banner">The origins of Ming River</div>
+    <h2 class="h1">Our Story</h2>
+    <span class="sub-title">Ming River is the original Sichuan baijiu. Using techniques passed down from master to apprentice for twenty-four generations, it is an authentic and uncompromising Chinese grain spirit. Discover a land of fiery flavors. Taste tradition.</span>
+
+    <div class="feat-img">
+      <span class="badge"><img src="assets/svg/ourstory-badge.svg"></span>
+      <img src="assets/placeholders/cocktail-img.jpg">
+    </div>
+
+    <div class="text-block">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in sem interdum diam feugiat pretium eget et velit. Vivamus eleifend at ex non auctor. Proin nec orci sollicitudin, maximus dui posuere, pretium arcu. Ut maximus eget ligula ac eleifend.</p>
+
+      <p>Proin fermentum porta scelerisque. Pellentesque consequat purus vitae justo placerat, a viverra sem interdum. Maecenas porttitor metus eu turpis rhoncus dignissim.</p>
+    </div>
+
+    <a href="" class="arrow-dwn"></a>
+  </div>
+
+</div>
+<!--End: Section Two-->
+
+<img src="assets/png/Background-001.png" class="bg01">
+
+<!--Start: Section Three-->
+<div class="home-section section-three">
+
+  <div class="content-block">
+    <div class="scroll-banner">The origins of Ming River</div>
+    <h3 class="h1">About Baijiu</h3>
+    <span class="sub-title">Ming River is the original Sichuan baijiu. Using techniques passed down from master to apprentice for twenty-four generations, it is an authentic and uncompromising Chinese grain spirit. Discover a land of fiery flavors. Taste tradition.</span>
+
+    <div class="text-block">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in sem interdum diam feugiat pretium eget et velit. Vivamus eleifend at ex non auctor. Proin nec orci sollicitudin, maximus dui posuere, pretium arcu. Ut maximus eget ligula ac eleifend.</p>
+    </div>
+
+    <a href="" class="solid-btn solid-btn--red">Read More</a>
+
+    <a href="" class="arrow-dwn"></a>
+  </div>
+
+</div>
+<!--End: Section Three-->
+
+<img src="assets/png/Background-002.png" class="bg02">
+
+<!--Start: Section Four-->
+<div class="home-section section-four">
+
+  <div class="content-block">
+    <div class="scroll-banner">Where Ming River is Made</div>
+    <h4 class="h1">The Distillery</h4>
+    <span class="sub-title">Small Excerpt about the Distillery Here</span>
+
+    <div class="feat-img">
+      <img src="assets/placeholders/cocktail-img.jpg">
+    </div>
+
+    <div class="text-block">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in sem interdum diam feugiat pretium eget et velit. Vivamus eleifend at ex non auctor. Proin nec orci sollicitudin, maximus dui posuere, pretium arcu. Ut maximus eget ligula ac eleifend.</p>
+    </div>
+
+    <a href="" class="solid-btn solid-btn--red">Learn More</a>
+
+    <a href="" class="arrow-dwn"></a>
+  </div>
+
+</div>
+<!--End: Section Four-->
+
+<img src="assets/png/Background-003.png" class="bg03">
+
+<!--Start: Section Five-->
+<div class="home-section section-five">
+
+  <div class="content-block">
+    <div class="scroll-banner">Latest News</div>
+    <h5 class="h1">Articles</h5>
+    <span class="sub-title">Small Excerpt about the Distillery Here</span>
+  </div>
+
+  <div class="grid-row grid-row__3block">
+    <div class="grid-item">
+      <a href="" class="category-tag">Articles</a>
+      <div class="content-block">
+        <a href="" class="h4">Beginners Guide to Baijiu</a>
+      </div>
+      <span class="cover"></span>
+      <img src="assets/placeholders/small-article-img1.png">
+    </div>
+
+    <div class="grid-item">
+      <a href="" class="category-tag">Articles</a>
+      <div class="content-block">
+        <a href="" class="h4">Beginners Guide to Baijiu</a>
+      </div>
+      <span class="cover"></span>
+      <img src="assets/placeholders/small-article-img1.png">
+    </div>
+
+    <div class="grid-item">
+      <a href="" class="category-tag">Articles</a>
+      <div class="content-block">
+        <a href="" class="h4">Beginners Guide to Baijiu</a>
+      </div>
+      <span class="cover"></span>
+      <img src="assets/placeholders/small-article-img1.png">
+    </div>
+  </div>
+
+  <div class="content-block">
+    <a href="" class="solid-btn solid-btn--red">View Articles</a>
+
+    <a href="" class="arrow-dwn"></a>
+  </div>
+
+</div>
+<!--End: Section Five-->
+
+<img src="assets/png/Background-004.png" class="bg04">
+
+<!--Start: Section Five-->
+<div class="home-section section-six">
+  <div class="content-block">
+    <h6 class="h1 text--uppercase">Contact Ming River</h6>
+
+    <ul class="socials">
+      <li><a href="https://www.instagram.com/mingriverbaijiu" class="instagram"></a></li>
+      <li><a href="https://www.facebook.com/Mingriverbaijiu/" class="facebook"></a></li>
+      <li><a href="https://twitter.com/mingriverbaijiu" class="twitter"></a></li>
+    </ul>
+
+    <a href="" class="solid-btn solid-btn--red">Email Us</a>
+  </div>
+
+  <img src="assets/png/BG-Footer.png" class="footer-img">
+</div>
+
+</section>
 
 <?php
 include('components/footer.php');
